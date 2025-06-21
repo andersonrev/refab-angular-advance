b350e34c2ffa77780e4117e3f92ccfb6feb43497
andersonrev <fabiander_21@hotmail.es>
2025-06-21 14:18:33 -0500

    avance av

---
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
+
+@Component({
+  selector: 'calculator-button',
+  standalone: true,
+  imports: [],
+  styleUrl: './calculator-button.component.css',
+  templateUrl: './calculator-button.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: "w-1/4 border-r border-b border-indigo-400"
+  }
+
+})
+export class CalculatorButtonComponent {
+
+    public isCommand = input(false, {
+      transform: (value: boolean | string) => 
+        typeof value === 'string' ? value === '' : value
+    })
+
+    @HostBinding('class.bg-indigo-700') get commandStyle() {
+      return this.isCommand();
+    }
+
+}
---
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component, HostBinding, input, signal } from '@angular/core';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+
+@Component({
+  selector: 'calculator',
+  standalone: true,
+  imports: [CalculatorButtonComponent],
+  templateUrl: './calculator.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CalculatorComponent {
+ }
