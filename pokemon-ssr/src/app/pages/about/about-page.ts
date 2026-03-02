fd311f68df8df83d90639e396c5295e67a199fe4
andersonrev <fabiander_21@hotmail.es>
2026-03-01 20:14:01 -0500

    feat: creacion de componentes y ruteado

---
+++ pokemon-ssr/src/app/pages/about/about-page.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-page',
+  imports: [],
+  templateUrl: './about-page.html',
+  styleUrl: './about-page.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class AboutPage {
+  
+
+}
