903f9c0124282ff991d9f271afd6b8c2fe7e6f36
andersonrev <fabiander_21@hotmail.es>
2026-05-16 17:02:37 -0500

    feat: 75 queryParams observables to signals

--- pokemon-ssr/src/app/pages/pokemons/pokemons.ts
+++ pokemon-ssr/src/app/pages/pokemons/pokemons.ts
@@ -3,6 +3,9 @@ import { PokemonList } from "./components/pokemon-list/pokemon-list";
 import { PokemonListSkeleton } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton";
 import { PokemonService } from '../../pokemons/services/pokemon-service';
 import { SimplePokemon } from '../../pokemons/interfaces';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop'
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-pokemons',
@@ -16,12 +19,24 @@ export default class Pokemons implements OnInit{
 
   private pokemonService = inject(PokemonService)
   public pokemons = signal<SimplePokemon[]>([])
-  // public isLoading = signal(true);
+
+  private route = inject(ActivatedRoute)
+
+  public currentPage = toSignal<number>(
+    this.route.queryParamMap.pipe(
+      map( params => params.get('page') ?? '1'),
+      map(page => ( isNaN(+page) ? 1 : +page)),
+      map( page => Math.max(1, page))
+    )
+  )
+    // public isLoading = signal(true);k
   // private appRef = inject(ApplicationRef)
 
   // private $appState = this.appRef.isStable.subscribe(isStable => { console.log({isStable})})
 
   ngOnInit(): void {
+    // this.route.queryParamMap.subscribe(console.log)
+    console.log(this.currentPage())
     this.loadPokemons()
     // setTimeout(() => {
     //   this.isLoading.set(false);
@@ -29,7 +44,10 @@ export default class Pokemons implements OnInit{
   }
 
   public loadPokemons (page = 0 ){
-    this.pokemonService.loadPage(page).subscribe(
+
+    const pageToLoad = this.currentPage() ! + page;
+
+    this.pokemonService.loadPage(pageToLoad).subscribe(
       pokemons => {
         this.pokemons.set(pokemons)
       }
